ad8df19095fc60aad7969fff00ef192e94d18635
Tatiana Limongi <117868187+tatilimongi@users.noreply.github.com>
2022-11-29 21:43:49 -0300

    Delete finalnext directory

--- finalnext/src/main/java/br/org/cesar/finalnext/repositories/ParecerRepository.java
+++
@@ -1,9 +0,0 @@
-package br.org.cesar.finalnext.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.org.cesar.finalnext.entities.Parecer;
-
-public interface ParecerRepository extends JpaRepository<Parecer, Long> {
-
-}
